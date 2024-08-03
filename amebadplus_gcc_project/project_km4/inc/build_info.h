b1425cd86c5388e523f75d98c6cdbce0d586912e
Zeng <zxf1023818103@gmail.com>
2024-08-03 05:26:12 +0000

    更新 gitignore

--- amebadplus_gcc_project/project_km4/inc/build_info.h
+++
@@ -1,7 +0,0 @@
-#define UTS_VERSION "2024/07/31-14:53:23"
-#define RTL_FW_COMPILE_TIME "2024/07/31-14:53:23"
-#define RTL_FW_COMPILE_DATE "2024/07/31"
-#define RTL_FW_COMPILE_BY "auto_jenkins_wlan5"
-#define RTL_FW_COMPILE_HOST ""
-#define RTL_FW_COMPILE_DOMAIN ""
-#define RTL_FW_COMPILER "gcc version 10.3.1 20220117 (Realtek ASDK-10.3.1 Build 4072+) "
